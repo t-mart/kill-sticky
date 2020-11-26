3e38b51813100264acc2d51a1a97c13f5c356b43
Imre Kószó <imrekoszo@users.noreply.github.com>
2020-11-26 12:17:47 +0100

    Make all hidden overflows visible

--- src/kill-sticky.js
+++ src/kill-sticky.js
@@ -6,6 +6,13 @@
         }
     });
 
+    document.querySelectorAll('html *').forEach(function(node) {
+        var s = getComputedStyle(node);
+        if ('hidden' === s['overflow']) { node.style['overflow'] = 'visible'; }
+        if ('hidden' === s['overflow-x']) { node.style['overflow-x'] = 'visible'; }
+        if ('hidden' === s['overflow-y']) { node.style['overflow-y'] = 'visible'; }
+    });
+
     var htmlNode = document.querySelector('html');
     htmlNode.style['overflow'] = 'visible';
     htmlNode.style['overflow-x'] = 'visible';
